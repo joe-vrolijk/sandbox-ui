e5a82a9270bf88ba15cfc7e190ef3dd8c8109b2d
Joe Vrolijk <joe.vrolijk@gmail.com>
2024-12-03 22:17:14 +0100

    WIP ChucksWisdom

--- app/(main)/chuck/page.tsx
+++ app/(main)/chuck/page.tsx
@@ -7,18 +7,8 @@ export default function Dashboard() {
         <div className="flex items-center">
           <h1 className="text-lg font-semibold md:text-2xl">Chuck</h1>
         </div>
-        <div
-          className="flex flex-1 items-center justify-center rounded-lg border border-dashed shadow-sm"
-          x-chunk="dashboard-02-chunk-1"
-        >
-          <div className="flex flex-col items-center gap-1 text-center">
-            <h3 className="text-2xl font-bold tracking-tight">
-              You have no content
-            </h3>
-            <p className="text-sm text-muted-foreground">Add some content!</p>
-            <Button className="mt-4">Some Button</Button>
-          </div>
-        </div>
+
+        
       </main>
     </>
   );
